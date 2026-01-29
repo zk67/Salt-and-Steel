2f3c77598be3d3cf4f9ef65fdc65a0fa1cbf047f
imabd <imane.abdeljalil123456@gmail.com>
2026-01-29 15:13:41 -0500

    feat(home): créer la page d'accueil
    Image Pirate
    
    Enlever RouterModule

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -1,50 +1,13 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CommunicationService } from '@app/services/communication.service';
-import { Message } from '@common/classes/message';
-import { BehaviorSubject } from 'rxjs';
-import { map } from 'rxjs/operators';
-
 @Component({
     selector: 'app-main-page',
     templateUrl: './main-page.component.html',
     styleUrls: ['./main-page.component.scss'],
     imports: [RouterLink],
 })
+
 export class MainPageComponent {
     readonly title: string = 'Salt and Steel';
-    message: BehaviorSubject<string> = new BehaviorSubject<string>('');
-
-    constructor(private readonly communicationService: CommunicationService) {}
-
-    sendTimeToServer(): void {
-        const newTimeMessage: Message = {
-            title: 'Hello from the client',
-            body: 'Time is : ' + new Date().toString(),
-        };
-        // Important de ne pas oublier "subscribe" ou l'appel ne sera jamais lancé puisque personne l'observe
-        this.communicationService.basicPost(newTimeMessage).subscribe({
-            next: (response) => {
-                const responseString = `Le serveur a reçu la requête a retourné un code ${response.status} : ${response.statusText}`;
-                this.message.next(responseString);
-            },
-            error: (err: HttpErrorResponse) => {
-                const responseString = `Le serveur ne répond pas et a retourné : ${err.message}`;
-                this.message.next(responseString);
-            },
-        });
-    }
-
-    getMessagesFromServer(): void {
-        this.communicationService
-            .basicGet()
-            // Cette étape transforme l'objet Message en un seul string
-            .pipe(
-                map((message: Message) => {
-                    return `${message.title} ${message.body}`;
-                }),
-            )
-            .subscribe(this.message);
-    }
+    iconeSrc: string = 'assets/imagePirate.png';
 }
