684e84fc8a0369cafda1d28e32b567c0d3d1249a
KuraiSainan <kuraisainan@gmail.com>
2026-02-05 14:17:39 -0500

    fix(dev): corriger un petit bug sur la dev

--- client/src/main.ts
+++ client/src/main.ts
@@ -7,7 +7,6 @@ import { CharacterPageComponent } from '@app/pages/character-page/character-page
 import { EditionPageComponent } from '@app/pages/edition-page/edition-page.component';
 import { FormEditionPageComponent } from '@app/pages/form-edition-page/form-edition-page.component';
 import { GameCreationPageComponent } from '@app/pages/game-creation-page/game-creation-page.component';
-import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { WaitingPageComponent } from '@app/pages/waiting-page/waiting-page.component';
@@ -22,7 +21,6 @@ if (environment.production) {
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
-    { path: 'game', component: GamePageComponent },
     { path: 'game-creation', component: GameCreationPageComponent },
     { path: 'material', component: MaterialPageComponent },
     { path: 'form-edition', component: FormEditionPageComponent },
