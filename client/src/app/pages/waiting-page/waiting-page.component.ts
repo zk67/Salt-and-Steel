11c2963589cbed394b4eb51a597cca7f991668ec
OlivierRi <richecraft@gmail.com>
2026-02-22 18:20:24 -0500

    feat(liste): ajout d'une liste de joueur temporaire

--- client/src/app/pages/waiting-page/waiting-page.component.ts
+++ client/src/app/pages/waiting-page/waiting-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Player } from '@common/types/player.interface';
 
 @Component({
     selector: 'app-waiting-page',
@@ -8,5 +9,66 @@ import { RouterLink } from '@angular/router';
     imports: [RouterLink],
 })
 export class WaitingPageComponent {
+    // player temporaire (va bugger quand il y aura une merge de l'interface de Player)
+    player1: Player = {
+        id: '1',
+        name: 'Player 1',
+        x: 5,
+        y: 5,
+        energy: 5,
+        speed: 6,
+        imageUrl: 'assets/avatars/avatar-1.png',
+    };
 
+    player2: Player = {
+        id: '2',
+        name: 'Player 2',
+        x: 5,
+        y: 5,
+        energy: 5,
+        speed: 6,
+        imageUrl: 'assets/avatars/avatar-2.png',
+    };
+
+    player3: Player = {
+        id: '3',
+        name: 'Player 3',
+        x: 5,
+        y: 5,
+        energy: 5,
+        speed: 6,
+        imageUrl: 'assets/avatars/avatar-3.png',
+    };
+
+    player4: Player = {
+        id: '4',
+        name: 'Player 4',
+        x: 5,
+        y: 5,
+        energy: 5,
+        speed: 6,
+        imageUrl: 'assets/avatars/avatar-4.png',
+    };
+
+    player5: Player = {
+        id: '5',
+        name: 'Player 5',
+        x: 5,
+        y: 5,
+        energy: 5,
+        speed: 6,
+        imageUrl: 'assets/avatars/avatar-5.png',
+    };
+
+    player6: Player = {
+        id: '6',
+        name: 'Player 6',
+        x: 5,
+        y: 5,
+        energy: 5,
+        speed: 6,
+        imageUrl: 'assets/avatars/avatar-6.png',
+    };
+
+    players = [this.player1, this.player2, this.player3, this.player4, this.player5, this.player6];
 }
