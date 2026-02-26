cbe18651311359ae219118725cf964ca35660d84
Aurelio-99 <wentedxiii@gmail.com>
2026-02-26 16:21:52 +0000

    feat(vue-de-jeux): Ajouter la structure pour la vue de jeux

---
+++ client/src/app/components/game/right-sidebar/right-sidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-right-sidebar',
+    templateUrl: './right-sidebar.component.html',
+    styleUrl: './right-sidebar.component.scss',
+    imports: [],
+})
+export class RightSidebarComponent {}
