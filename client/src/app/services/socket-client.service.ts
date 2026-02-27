e7fdb3c0d3ad0090dc080a458f8a95cc267828f2
Zakaria <zakaria.soudaki@yahoo.ca>
2026-02-27 01:17:47 -0500

    fix : modification import le bon url

--- client/src/app/services/socket-client.service.ts
+++ client/src/app/services/socket-client.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({ providedIn: 'root' })
 export class SocketClientService {
