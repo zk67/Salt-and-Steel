cb6ccf4202d0a1645436b53f2fdd5b254fec99f3
imabd <imane.abdeljalil123456@gmail.com>
2026-03-11 10:28:17 -0400

    feat(join-game): afficher la liste initiale des parties joignables

--- client/src/app/pages/join-game-page/join-game-page.component.ts
+++ client/src/app/pages/join-game-page/join-game-page.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SocketClientService } from '@app/services/socket-client.service';
+import { Game } from '@common/types/game.interface';
+
+interface JoinableGame {
+    roomId: string;
+    game: Game;
+    playerCount: number;
+}
 @Component({
     selector: 'join-game-page',
     templateUrl: './join-game-page.component.html',
@@ -7,5 +15,25 @@ import { RouterLink } from '@angular/router';
     imports: [RouterLink],
 })
 
-export class JoinGameComponent {
+export class JoinGameComponent implements OnInit, OnDestroy {
+    joinableGames: JoinableGame[] = [];
+
+    private readonly onJoinableGames = (games: JoinableGame[]) => {
+        this.joinableGames = games;
+    };
+
+    constructor(private socketService: SocketClientService) {}
+
+    ngOnInit(): void {
+        if (!this.socketService.isSocketAlive()) {
+            this.socketService.connect();
+        }
+
+        this.socketService.on<JoinableGame[]>('joinableGames', this.onJoinableGames);
+        this.socketService.send('getJoinableGames');
+    }
+
+    ngOnDestroy(): void {
+        this.socketService.off('joinableGames', this.onJoinableGames);
+    }
 }
