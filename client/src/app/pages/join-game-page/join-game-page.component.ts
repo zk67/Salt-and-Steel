3a1d3d664b62bc6b5a3e029926e69eee2c8c42eb
imabd <imane.abdeljalil123456@gmail.com>
2026-03-11 09:37:47 -0400

    feat(join-game): add join game page and route from main page

---
+++ client/src/app/pages/join-game-page/join-game-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+@Component({
+    selector: 'join-game-page',
+    templateUrl: './join-game-page.component.html',
+    styleUrls: ['./join-game-page.component.scss'],
+    imports: [RouterLink],
+})
+
+export class JoinGameComponent {
+}
